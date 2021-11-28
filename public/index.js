dd8b8a2ebaa94041fde24034a9b8c134abb93fc8
= <phathist494@gmail.com>
2021-11-28 15:05:38 +0700

    secondsubmit

--- public/index.js
+++ public/index.js
@@ -1,41 +1,19 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-const auth = getAuth();
 
+const auth = getAuth();
 onAuthStateChanged(auth, (user) => {
-   if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
-      document.getElementById("user_div ").style.display = "block";
-      document.getElementById("login_div").style.display = "none";
-      window.alert("ถูกละมั้ง")
-      const uid = user.uid;
-    // ...
-  }else {
-      document.getElementById("user_div").style.display = "none";
-      document.getElementById("login_div").style.display = "block";
-      // User is signed out
-      // ...
-      window.alert("ถูกละมั้ง")
+  if (user) {
+    location.replace("welcome.html")
   }
 });
 
-function login() {
 
-  let userEmail = document.getElementById("email_field").value;
-  let userPass = document.getElementById("password_field").value;
+function login(){
 
-  createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
+  const userEmail = document.getElementById("email").value;
+  const userPass = document.getElementById("password").value;
 
-    window.alert("Error : " + errorMessage)
-  });
+  firebase.auth().signInWIthEmailAndPassword()
+  
 
 }
--- public/welcome.js
+++ public/welcome.js
@@ -1,22 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, onAuthStateChanged, signInWithEmailAndPassword} from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-apiKey: "AIzaSyCGmzsQhbDhih4mchg4bZ--XIgoUVil-L8",
-authDomain: "fir-new-1c02e.firebaseapp.com",
-databaseURL: "https://fir-new-1c02e-default-rtdb.firebaseio.com",
-projectId: "fir-new-1c02e",
-storageBucket: "fir-new-1c02e.appspot.com",
-messagingSenderId: "443312480447",
-appId: "1:443312480447:web:208711501d8a77e52de60a",
-measurementId: "G-D8F8ET1S7T"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+      location.replace("index.html");
+  }
+});
